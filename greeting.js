b6b8a36b94203dafdfa301e929a96fe7028e7f81
MarkKim1 <46219125+MarkKim1@users.noreply.github.com>
2020-01-19 00:34:38 +0900

    saving ToDo
    fixed an error on greeting.js

--- greeting.js
+++ greeting.js
@@ -5,16 +5,21 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const currentValue = input.value;
-//   paintGreeting(currentValue);
-// }
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
 
-// function askForName() {
-//   form.classList.add(SHOWING_CN);
-//   form.addEventListener("submit", handleSubmit);
-// }
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
@@ -25,7 +30,7 @@ function paintGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    // askForName();
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
